d731a680dd1c1dc846d81f4a7bfd850088a8dcc8
strukteon <sensentod@gmail.com>
2018-02-24 15:47:30 +0100

    Improved SQL loading time

--- src/main/java/utils/UserSQL.java
+++ src/main/java/utils/UserSQL.java
@@ -13,17 +13,15 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class UserSQL {
 
     public List<Column> rowData;
 
-    private MysqlDataSource source;
-    private static MysqlDataSource srource;
+    private static MysqlDataSource source;
+
+    private static Connection c;
 
     private String table;
 
@@ -31,11 +29,11 @@ public class UserSQL {
         this.source = source;
     }
 
-    public static MysqlDataSource login(String sqluser, String password, String database, String server){
+    public static MysqlDataSource login(String sqluser, String password, String database, String server) throws Exception{
         return login(sqluser, password, database, server, 3306);
     }
 
-    public static MysqlDataSource login(String sqluser, String password, String database, String server, int port){
+    public static MysqlDataSource login(String sqluser, String password, String database, String server, int port) throws Exception{
         MysqlDataSource source = new MysqlDataSource();
 
         source.setUser(sqluser);
@@ -44,6 +42,22 @@ public class UserSQL {
         source.setServerName(server);
         source.setPort(port);
         source.setDatabaseName(database);
+
+        c = source.getConnection();
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    Connection ctemp = source.getConnection();
+                    c.close();
+                    c = ctemp;
+                } catch (SQLException e){
+                    e.printStackTrace();
+                }
+
+            }
+        }, 0, 20000*1000);
 
         return source;
     }
@@ -65,27 +79,21 @@ public class UserSQL {
 
 
     public boolean tableExists(String table) throws Exception {
-        try (Connection c = source.getConnection()) {
-            ResultSet set = c.createStatement().executeQuery("select * from " + table);
-            return true;
-        } catch (MySQLSyntaxErrorException e) {
-            return false;
-        }
+        ResultSet set = c.createStatement().executeQuery("select * from " + table);
+        return true;
     }
 
     public boolean createTableIfNotExists() throws Exception {
         if (!tableExists(table)){
-            try (Connection c = source.getConnection()) {
-                String columnsSql = "";
-                for (int i = 0; i < rowData.size(); i++) {
-                    if (i != 0)
-                        columnsSql += ", ";
-                    columnsSql += rowData.get(i).variable + " " + rowData.get(i).datatype;
-                }
-
-                c.createStatement().executeUpdate("create table " + table + " ( " + columnsSql + " )");
-                return true;
+            String columnsSql = "";
+            for (int i = 0; i < rowData.size(); i++) {
+                if (i != 0)
+                    columnsSql += ", ";
+                columnsSql += rowData.get(i).variable + " " + rowData.get(i).datatype;
             }
+
+            c.createStatement().executeUpdate("create table " + table + " ( " + columnsSql + " )");
+            return true;
         }
         return false;
     }
@@ -95,52 +103,48 @@ public class UserSQL {
 
         boolean altered = false;
 
-        try (Connection c = source.getConnection()) {
-            ResultSet set = c.createStatement().executeQuery("select column_name, data_type from information_schema.columns where table_name='" + table + "';");
-            HashMap<String, String> columnsOnline = new HashMap<>();
-            while (set.next()){
-                columnsOnline.put(set.getString("column_name"), set.getString("data_type"));
-            }
-
-            // remove column from both list if they overlap
-
-            List<Column> rowDataTemp = new ArrayList<>(rowData);
-
-            for (int i = 0; i < rowData.size(); i++){
-
-                if ((columnsOnline.get(rowData.get(i).variable) == null ? "" : columnsOnline.get(rowData.get(i).variable)).equals(rowData.get(i).datatype)){
-                    columnsOnline.remove(rowData.get(i).variable);
-                    rowDataTemp.remove(rowData.get(i));
-                }
-
-            }
-
-            altered = rowDataTemp.size() != 0 || ( columnsOnline.size() != 0 && deleteMissing );
-
-            // remove remaining columns from columnsonline
-            if (deleteMissing)
-                for (String s : columnsOnline.keySet()){
-                    c.createStatement().executeUpdate("alter table " + table + " drop column " + s);}
-
-            //add remaining columns from rowdatatemp
-            for (Column col : rowDataTemp)
-                c.createStatement().executeUpdate("alter table " + table + " add column " + col.variable + " " + col.datatype);
-
-            return altered;
+        ResultSet set = c.createStatement().executeQuery("select column_name, data_type from information_schema.columns where table_name='" + table + "';");
+        HashMap<String, String> columnsOnline = new HashMap<>();
+        while (set.next()){
+            columnsOnline.put(set.getString("column_name"), set.getString("data_type"));
         }
+
+        // remove column from both list if they overlap
+
+        List<Column> rowDataTemp = new ArrayList<>(rowData);
+
+        for (int i = 0; i < rowData.size(); i++){
+
+            if ((columnsOnline.get(rowData.get(i).variable) == null ? "" : columnsOnline.get(rowData.get(i).variable)).equals(rowData.get(i).datatype)){
+                columnsOnline.remove(rowData.get(i).variable);
+                rowDataTemp.remove(rowData.get(i));
+            }
+
+        }
+
+        altered = rowDataTemp.size() != 0 || ( columnsOnline.size() != 0 && deleteMissing );
+
+        // remove remaining columns from columnsonline
+        if (deleteMissing)
+            for (String s : columnsOnline.keySet()){
+                c.createStatement().executeUpdate("alter table " + table + " drop column " + s);}
+
+        //add remaining columns from rowdatatemp
+        for (Column col : rowDataTemp)
+            c.createStatement().executeUpdate("alter table " + table + " add column " + col.variable + " " + col.datatype);
+
+        return altered;
+
     }
 
 
     public boolean userExists(String key, String value) throws Exception {
 
-        try (Connection c = source.getConnection()) {
+        ResultSet set = c.createStatement().executeQuery("select " + key + " from " + table + " where " + key + "='" + value + "'");
 
-            ResultSet set = c.createStatement().executeQuery("select " + key + " from " + table + " where " + key + "='" + value + "'");
-
-            while (set.next())
-                if (set.getString(key).equals(value))
-                    return true;
-        }
+        while (set.next())
+            if (set.getString(key).equals(value))
+                return true;
 
         return false;
     }
@@ -148,62 +152,57 @@ public class UserSQL {
     public boolean createUserIfNotExists(String key, String value) throws Exception {
         if (userExists(key, value))
             return false;
-        else
-            try (Connection c = source.getConnection()) {
-                String variables = "";
-                for (Column column : rowData){
-                    if (!column.equals(rowData.get(0)))
-                        variables += ", ";
-                    variables += column.variable;
-                }
-
-                String values = "";
-                for (Column column : rowData){
-                    if (!column.equals(rowData.get(0)))
-                        values += ", ";
-                    if (column.variable.equals(key))
-                        values += value;
-                    else
-                        values += "'" + column.defaultValue + "'";
-                }
-
-                c.createStatement().executeUpdate("insert into " + table + " ( " + variables + " ) values ( " + values + " )");
-                return true;
+        else {
+            String variables = "";
+            for (Column column : rowData){
+                if (!column.equals(rowData.get(0)))
+                    variables += ", ";
+                variables += column.variable;
             }
+
+            String values = "";
+            for (Column column : rowData){
+                if (!column.equals(rowData.get(0)))
+                    values += ", ";
+                if (column.variable.equals(key))
+                    values += value;
+                else
+                    values += "'" + column.defaultValue + "'";
+            }
+
+            c.createStatement().executeUpdate("insert into " + table + " ( " + variables + " ) values ( " + values + " )");
+            return true;
+        }
 
     }
 
     public void updateUser(String key, String value, Column.Change... columns) throws Exception {
-        if (!userExists(key, value))
-            return;
-        else
-            try (Connection c = source.getConnection()) {
-                String changes = "";
+        if (userExists(key, value)) {
+            String changes = "";
 
-                for (Column.Change column : columns){
-                    if (!column.variable.equals(columns[0].variable))
-                        changes += ", ";
-                    changes += column.variable + "='" + column.newValue + "'";
-                }
-
-                c.createStatement().executeUpdate("update " + table + " set " + changes + " where " + key + "='" + value + "'");
+            for (Column.Change column : columns){
+                if (!column.variable.equals(columns[0].variable))
+                    changes += ", ";
+                changes += column.variable + "='" + column.newValue + "'";
             }
+
+            c.createStatement().executeUpdate("update " + table + " set " + changes + " where " + key + "='" + value + "'");
+        }
     }
 
     public HashMap<String, String> getUser(String key, String value) throws Exception {
         if (!userExists(key, value))
             return null;
-        else
-            try (Connection c = source.getConnection()) {
-                HashMap<String, String> map = new HashMap<>();
-                ResultSet set = c.createStatement().executeQuery("select * from " + table + " where " + key + "='" + value + "'");
-                set.first();
-                for (Column column : rowData){
-                    String val = (set.getString(column.variable) != null ? set.getString(column.variable) : rowData.get(rowData.indexOf(column)).defaultValue);
-                    map.put(column.variable, val);
-                }
-                return map;
+        else {
+            HashMap<String, String> map = new HashMap<>();
+            ResultSet set = c.createStatement().executeQuery("select * from " + table + " where " + key + "='" + value + "'");
+            set.first();
+            for (Column column : rowData) {
+                String val = (set.getString(column.variable) != null ? set.getString(column.variable) : rowData.get(rowData.indexOf(column)).defaultValue);
+                map.put(column.variable, val);
             }
+            return map;
+        }
     }
 
 
