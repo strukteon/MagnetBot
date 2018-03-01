cc38c8e0a618db7cd98940164d6ed39c63a71513
strukteon <sensentod@gmail.com>
2018-03-01 23:57:03 +0100

    updated repeat command

--- src/main/java/commands/chat/commands/music/Repeat.java
+++ src/main/java/commands/chat/commands/music/Repeat.java
@@ -24,16 +24,16 @@ public class Repeat implements ChatCommand {
     public void action(MessageReceivedEvent event, String full, String cmd, String[] args) throws Exception {
         TrackScheduler scheduler = Main.audioCore.getGuildAudioPlayer(event.getGuild()).scheduler;
 
-        if (scheduler.getQueue().size() == 0){
+        if (scheduler.getQueue().size() == 0 && scheduler.getCurrentTrack() == null){
             event.getTextChannel().sendMessage(Message.ERROR(event, "Queue is empty!").build()).queue();
         } else {
 
             if (scheduler.repeat){
                 scheduler.repeatQueue(false);
-                event.getTextChannel().sendMessage(Message.INFO(event, "The current queue is now repeating!").build()).queue();
+                event.getTextChannel().sendMessage(Message.INFO(event, "The current queue is no longer repeating!").build()).queue();
             } else {
                 scheduler.repeatQueue(true);
-                event.getTextChannel().sendMessage(Message.INFO(event, "The current queue is no longer repeating!").build()).queue();
+                event.getTextChannel().sendMessage(Message.INFO(event, "The current queue is now repeating!").build()).queue();
             }
 
         }
