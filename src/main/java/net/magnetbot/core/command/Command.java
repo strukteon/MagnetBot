d58816c7d94e15fdcbf57386d09b9e5f8b3017f9
strukteon <mail@strukteon.me>
2018-06-10 17:07:00 +0200

    new command implementation

--- src/main/java/net/magnetbot/core/command/Command.java
+++ src/main/java/net/magnetbot/core/command/Command.java
@@ -5,6 +5,7 @@ package net.magnetbot.core.command;
     (c) nils 2018
 */
 
+import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.magnetbot.core.command.syntax.Syntax;
 
@@ -13,4 +14,12 @@ public interface Command {
         void action(MessageReceivedEvent event, Syntax syntax) throws Exception;
 
         Chat.CommandInfo commandInfo(); // banned = -10, guest = -1, member = 0, admin = 1, owner = 2, bot_owner = 3
+
+        default Permission[] requiredBotPerms(){
+            return new Permission[0];
+        }
+
+        default Permission[] requiredUserPerms(){
+            return new Permission[0];
+        }
 }
